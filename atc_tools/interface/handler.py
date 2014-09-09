59aa2b4295cdbfe6869f3a8a546a06de7896ba88
R. C. Thomas <rcthomas@lbl.gov>
2014-09-09 16:09:58 -0700

    Now support for test mode.

--- atc_tools/interface/handler.py
+++ atc_tools/interface/handler.py
@@ -8,18 +8,22 @@ class Handler ( object ) :
     def __init__( self, authenticator ) :
         self.authenticator = authenticator
 
-    def __call__( self, path, method = "GET", data = None, params = None ) :
+    def __call__( self, path, method = "GET", data = None, params = None, test_mode = False ) :
         """Assemble, sign, and send a request."""
 
-        return self._handle_request( path, method, data, params )
+        return self._handle_request( path, method, data, params, test_mode )
 
-    def _handle_request( self, path, method, data, params ) :
+    def _handle_request( self, path, method, data, params, test_mode ) :
         """Concrete subclasses provide an implementation.
 
         Headers sent must include
             Content-Type:       application/json (if request JSON data is being sent)
             ATC-User-ID:        (provided by authenticator)
             ATC-Request-Time:   (Unix time in milliseconds)
-            ATC-Signature:      (provided by authenticator, depends on final request URL."""
+            ATC-Signature:      (provided by authenticator, depends on final request URL.
+        Headers can also optionally include
+            ATC-Test_Mode:      (with a dummy value).
+        
+        Optional headers may have no effect for some methods."""
 
         raise NotImplementedError
