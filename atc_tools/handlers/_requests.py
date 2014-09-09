d75fcf12f4f86d478c5e69184d3954f74bc78075
R. C. Thomas <rcthomas@lbl.gov>
2014-09-09 16:09:10 -0700

    Support for test mode.

--- atc_tools/handlers/_requests.py
+++ atc_tools/handlers/_requests.py
@@ -14,7 +14,7 @@ class RequestsHandler ( interface.Handler ) :
     def __init__( self, authenticator ) :
         self.authenticator = authenticator
 
-    def _handle_request( self, path, method, data, params ) :
+    def _handle_request( self, path, method, data, params, test_mode ) :
 
         # Use the "prepared request" approach, because we need the exact
         # URL that requests is going to use to send the message for the 
@@ -36,6 +36,8 @@ class RequestsHandler ( interface.Handler ) :
         request.headers[ "ATC-User-ID"      ] = self.authenticator.user_id
         request.headers[ "ATC-Request-Time" ] = int( time.time() * 1.0e6 )
         request.headers[ "ATC-Signature"    ] = self.authenticator( request.headers[ "ATC-Request-Time" ], method, request.url, data )
+        if test_mode :
+            request.headers[ "ATC-Test-Mode" ] = 1
 
         # Send request and receive response.  Then intercept any errors and
         # translate them into an appropriate ATCException object if necessary.
