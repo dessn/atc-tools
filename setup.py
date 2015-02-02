a7d33ca3844f2526237d08c508bea6e223163b47
R. C. Thomas <rcthomas@lbl.gov>
2015-02-02 13:44:58 -0800

    Ah right, it is not like ExtUtils::MakeMaker.

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+from    distutils.core  import  setup
+import  os
+
+# Scripts to install on all systems.
+
+scripts = [ 
+        "atc-generate-keypair"  ,
+        "atc-ping-atc"          ,
+        "atc-register-atel"     ,
+        "atc-tag-aat"           ,
+        "atc-tag-simple"        ,
+        "atc-targets-for-aat"
+        ]
+
+# Additional scripts for NERSC installations.
+
+if "NERSC_HOST" in os.environ :
+    scripts += [ "atc-post-aat-spectra" ]
+
+# Package setup.  The requirements may be too stringent and older versions
+# may be alright.  Haven't checked.
+
+setup(  name            =   "atc-tools"                                     ,
+        version         =   "0.98"                                          ,
+        description     =   "Python ATC client reference implementation."   ,
+        author          =   "R. C. Thomas"                                  ,
+        author_email    =   "rcthomas@lbl.gov"                              ,
+        url             =   "https://github.com/dessn/atc-tools"            ,
+        requires        =   [ "rsa (>=3.1.4)", "requests (>=2.4.1)" ]       ,
+        packages        =   [ "atc_tools" ]                                 ,
+        scripts         =   scripts                                         )
